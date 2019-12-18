034a7d1a8e707ce79074bfa9f75d46413ed48a55
TiberioBrasil <tiberiobrasil@gmail.com>
2019-12-18 17:25:56 -0300

    :art: Criaçao de entidades matricula, planos e checkin

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -65,7 +65,6 @@ class StudentController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      console.log(req.body);
       return res.status(400).json({ error: 'Validation failed' });
     }
 
